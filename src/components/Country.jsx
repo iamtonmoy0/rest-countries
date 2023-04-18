e8fd846fd78514ba53999ab2a2a3f5bbbc36651a
iamtonmoy0 <tonmoy2code@gmail.com>
2023-04-18 01:10:13 -0400

    modified

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 
 export const Country=(props)=>{
+	const {area,name,region}=props.country;
 	
 
 	return(
 		<>
-		<h2> Name: {props.country.name.common}</h2>
+		<h2> Name: {name.common}</h2>
+		<p>Area: {area}</p>
+		<p><small>Region: {region}</small></p>
 				</>
 	)
 }
